9c667dc697ecb618006dfa5ebdf1847aa71f1b7e
nikhilkawadkar <nikhil.kawadkar9860@gmail.com>
2022-05-10 11:57:15 +0530

    initial commit

---
+++ src/main/java/com/example/JenkinsGit/jenkinsGit/JenkinsGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsGit.jenkinsGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsGit/jenkinsGit/JenkinsGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsGit.jenkinsGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
